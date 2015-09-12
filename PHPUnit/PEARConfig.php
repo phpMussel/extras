2fdd1f98b6e607e415d72a1b223a41abe5d1b215
Daniel Ruf <DanielRuf@users.noreply.github.com>
2015-09-12 21:53:59 +0200

    use boolean

--- PHPUnit/PEARConfig.php
+++ PHPUnit/PEARConfig.php
@@ -1,31 +1,25 @@
 <?php
 
-require_once('Config.php');
+require_once 'Config.php';
 $phpIni = new Config();
-
 // read config file
 $root =& $phpIni->parseConfig('phpmussel.ini', 'inicommented');
-
 // get value from config file
 // $generalSection =& $root->getItem("section", "general");
 // $cleanup = $generalSection->getItem("directive", "cleanup"); 
 $cleanup = $root->getItem("section", "general")->getItem("directive", "cleanup"); 
 $disable_cli = $root->getItem("section", "general")->getItem("directive", "disable_cli");
-echo ($cleanup->getContent());
-echo ($disable_cli->getContent());
-
+var_dump((bool)$cleanup->getContent());
+var_dump((bool)$disable_cli->getContent());
 // set value in config file to false
-$cleanup->setContent("false");
-$disable_cli->setContent("true");
-
+$cleanup->setContent(false);
+$disable_cli->setContent(true);
 // get current value (not yet saved to the config file)
-echo ($cleanup->getContent());
-echo ($disable_cli->getContent());
-
+var_dump($cleanup->getContent());
+var_dump($disable_cli->getContent());
 // save new value to config file
 // $cleanup->writeConfig('phpmussel.ini', 'inicommented');
 $root->writeDatasrc('phpmussel.ini', 'inicommented');
-
 // get saved value from config file
-echo $phpIni->parseConfig('phpmussel.ini', 'inicommented')->getItem("section", "general")->getItem("directive", "cleanup")->getContent();
-echo $phpIni->parseConfig('phpmussel.ini', 'inicommented')->getItem("section", "general")->getItem("directive", "disable_cli")->getContent(); 
+var_dump((bool)$phpIni->parseConfig('phpmussel.ini', 'inicommented')->getItem("section", "general")->getItem("directive", "cleanup")->getContent());
+var_dump((bool)$phpIni->parseConfig('phpmussel.ini', 'inicommented')->getItem("section", "general")->getItem("directive", "disable_cli")->getContent());
