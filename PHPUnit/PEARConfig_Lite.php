5cdb24f72e51110beb06ab2514fb4972600f3bfa
Daniel Ruf <DanielRuf@users.noreply.github.com>
2015-09-12 21:37:57 +0200

    updated Config_Lite example

--- PHPUnit/PEARConfig_Lite.php
+++ PHPUnit/PEARConfig_Lite.php
@@ -7,16 +7,16 @@ $config = new Config_Lite();
 $config->read('phpmussel.ini');
 
 // get value from config file
-echo $config->get('general', 'cleanup');
-echo $config->get('general', 'disable_cli');
+var_dump($config->getBool('general', 'cleanup'));
+var_dump($config->getBool('general', 'disable_cli'));
 
 // set value in config file to false
 $config->set('general', 'cleanup',"false");
 $config->set('general', 'disable_cli',"true"); 
 
 // get current value (not yet saved to the config file)
-echo $config->get('general', 'cleanup');
-echo $config->get('general', 'disable_cli');
+var_dump($config->getBool('general', 'cleanup'));
+var_dump($config->getBool('general', 'disable_cli'));
 
 // save new value to config file
 $config->save();
@@ -25,4 +25,5 @@ $config->save();
 $config->read('phpmussel.ini');
 
 // get saved value from config file
-echo $config->read('phpmussel.ini')->get('general', 'cleanup'); 
+var_dump($config->read('phpmussel.ini')->getBool('general', 'cleanup'));
+var_dump($config->read('phpmussel.ini')->getBool('general', 'disable_cli'));
