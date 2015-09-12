201fc9652d1e5510d638dfb3bfde6288521f3d88
Daniel Ruf <DanielRuf@users.noreply.github.com>
2015-09-12 21:03:32 +0200

    updated example for PEAR Config

--- PHPUnit/PEARConfig.php
+++ PHPUnit/PEARConfig.php
@@ -10,17 +10,22 @@ $root =& $phpIni->parseConfig('phpmussel.ini', 'inicommented');
 // $generalSection =& $root->getItem("section", "general");
 // $cleanup = $generalSection->getItem("directive", "cleanup"); 
 $cleanup = $root->getItem("section", "general")->getItem("directive", "cleanup"); 
+$disable_cli = $root->getItem("section", "general")->getItem("directive", "disable_cli");
 echo ($cleanup->getContent());
+echo ($disable_cli->getContent());
 
-// set value in config file to 1
-$cleanup->setContent(1);
+// set value in config file to false
+$cleanup->setContent(false);
+$disable_cli->setContent(true);
 
 // get current value (not yet saved to the config file)
 echo ($cleanup->getContent());
+echo ($disable_cli->getContent());
 
 // save new value to config file
 // $cleanup->writeConfig('phpmussel.ini', 'inicommented');
 $root->writeDatasrc('phpmussel.ini', 'inicommented');
 
 // get saved value from config file
-echo $phpIni->parseConfig('phpmussel.ini', 'inicommented')->getItem("section", "general")->getItem("directive", "cleanup")->getContent(); 
+echo $phpIni->parseConfig('phpmussel.ini', 'inicommented')->getItem("section", "general")->getItem("directive", "cleanup")->getContent();
+echo $phpIni->parseConfig('phpmussel.ini', 'inicommented')->getItem("section", "general")->getItem("directive", "disable_cli")->getContent(); 
